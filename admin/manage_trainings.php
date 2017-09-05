cdf1159a6443c6610b69dce7142dac846230a331
donbee <jonathan.le2@uta.edu>
2017-09-05 01:05:46 -0500

    Training Module Part 1a of 2
    * When issuing certificate, only show devices or groups that have trainings
    * Changed Admin not logged in forwarding to admin/index.php
    
    Still to come, search for certificates by device or user

--- admin/manage_trainings.php
+++ admin/manage_trainings.php
@@ -5,9 +5,9 @@
  */
 include_once ($_SERVER['DOCUMENT_ROOT'].'/pages/header.php');
 
-if ($staff && $staff->getRoleID() < 7){
+if (!$staff || $staff->getRoleID() < 7){
     //Not Authorized to see this Page
-    header('Location: /index.php');
+    header('Location: index.php');
 }
 
 if($_SESSION['type'] == 'success'){
