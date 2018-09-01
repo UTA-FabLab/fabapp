663a92049168286d87d9e8c10f0768e59e8f83e0
Unknown <jonlecfp@gmail.com>
2018-09-01 11:34:00 -0500

    Minor Bug Fix
    Removed unnecessary code from getDevices

--- pages/sub/getDevices.php
+++ pages/sub/getDevices.php
@@ -10,7 +10,7 @@ if (!empty($_GET["val"])) {
 
         sscanf($value, "%d", $dg_id);
 
-        echo ("dg_id = $dg_id");
+        //echo ("dg_id = $dg_id");
 
         if ($dg_id !="" && $dg_id !="2" && DeviceGroup::regexDgID($dg_id)) {
             // Select all of the MAV IDs that are waiting for this device group
@@ -21,15 +21,13 @@ if (!empty($_GET["val"])) {
                 ORDER BY `device_desc`
             " );
 	
-            echo "<select>";
-            while($row = mysqli_fetch_array($result))
-            {
+            while($row = mysqli_fetch_array($result)) {
                 echo '<option value="'.$row["d_id"].'">'; echo $row["device_desc"]; echo "</option>";
             }
             if (mysqli_num_rows($result)==0) {
-            echo "<option disabled>"; echo "A Device Is Available To Use"; echo "</option>";  
+                echo ("<script>document.getElementById(\"deviceList\").disabled = true;</script>");
+                echo "<option selected disabled hidden>"; echo "A Device Is Available To Use"; echo "</option>";
             }
-            echo "</select>";
             
         
         }
@@ -41,16 +39,11 @@ if (!empty($_GET["val"])) {
                 ORDER BY `device_desc`
             " );
             
-            echo "<select>";
-            
             if (mysqli_num_rows($result) < 9) {
-                echo "<option disabled>"; echo "A Printer Is Available To Use"; echo "</option>";  
-            }
-            else{
+                echo "<option disabled selected>"; echo "A Printer Is Available To Use"; echo "</option>";  
+            } else {
                 echo "<option>"; echo "No Selection Needed"; echo "</option>";  
             }
-           
-            echo "</select>";
         }
     
 }
