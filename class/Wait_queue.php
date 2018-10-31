094308a358bdd0d4558ed81d90f0b3968e1271fa
sammyhamwi <sammyhamwi@gmail.com>
2018-10-31 16:29:02 -0500

    fixed issue with printing wait queue ticket reciepts

--- class/Wait_queue.php
+++ class/Wait_queue.php
@@ -92,6 +92,8 @@ class Wait_queue {
             ")){        
                 Notifications::sendNotification($mysqli->insert_id, "FabApp Notification", "You have signed up for FabApp notifications. Your Wait Ticket number is: ".$mysqli->insert_id."", 'From: FabApp Notifications' . "\r\n" .'', 0);
                 Wait_queue::calculateDeviceWaitTimes();
+                //Commented out for Dev purposes
+                //Wait_queue::printTicket($operator, $dg_id);
                 return $mysqli->insert_id;
                 
             } else {
@@ -107,10 +109,9 @@ class Wait_queue {
             ")){        
                 Notifications::sendNotification($mysqli->insert_id, "FabApp Notification", "You have signed up for FabApp notifications. Your Wait Ticket number is: ".$mysqli->insert_id."", 'From: FabApp Notifications' . "\r\n" .'', 0);
                 Wait_queue::calculateWaitTimes();
-                $q_id = $mysqli->insert_id;
-                //Disabled for Dev Purposes
-                //Wait_queue::printTicket($q_id);
-                return $q_id;
+                //Commented out for Dev purposes
+                //Wait_queue::printTicket($operator, $dg_id);
+                return $mysqli->insert_id;
             } else {
                 return ("<div class='alert alert-danger'>Error2 updating contact info!</div>");
             }
@@ -593,29 +594,32 @@ class Wait_queue {
     }
     
     
-    public static function printTicket($q_id){
+    public static function printTicket($operator, $dg_id){
         global $mysqli;
-        global $sv;
+        global $tp;
         $est_cost = 0;
-        
+
         if($result = $mysqli->query("
-            SELECT `wait_queue`.`estTime`, `devices`.`device_desc`
+            SELECT `wait_queue`.`estTime`, `wait_queue`.`Q_id`, `devices`.`device_desc`
             FROM `wait_queue`
             LEFT JOIN `devices`
             ON `wait_queue`.`Dev_id` = `devices`.`d_id`
-            WHERE `wait_queue`.`q_id` = $q_id
+            WHERE `wait_queue`.`Operator` = $operator AND `wait_queue`.`Devgr_id` = $dg_id
         ")){
             $row = $result->fetch_assoc();
             $timeLeft = $row["estTime"];
             $device = $row["device_desc"];
+            $q_id = $row["Q_id"];
+        }
+
+        if (empty($device)){
+            $device = "PolyPrinter";
         }
 
         // Set up Printer Connection
-        $tp_array = explode("|", $sv['thermalPrinter1']);
-        $tpHost = $tp_array[0];
-        $tpPort = $tp_array[1];
+        $tp_number = 0;
         try {
-            $connector = new NetworkPrintConnector( $tpHost, $tpPort);
+            $connector = new NetworkPrintConnector( $tp[$tp_number][0], $tp[$tp_number][1]);
             $printer = new Printer($connector);
         } catch (Exception $e) {
             return "Couldn't print to this printer: " . $e -> getMessage() . "\n";
