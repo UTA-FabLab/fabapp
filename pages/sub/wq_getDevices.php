8a7b617f8f101570787e9625d34087e13ecab27c
Jon Le <jonlecfp@gmail.com>
2018-11-09 13:07:37 -0600

    Simplified WQ Get Devices
    Reduced criteria for building out the device list, relying on DG:popDG_WQ() to do all the heavy lifting.  This will allow WQ tickets to be created even if the device doesn't have an active ticket.

--- pages/sub/wq_getDevices.php
+++ pages/sub/wq_getDevices.php
@@ -9,66 +9,31 @@ include_once ($_SERVER ['DOCUMENT_ROOT'] . '/connections/db_connect8.php');
 include_once ($_SERVER ['DOCUMENT_ROOT'] . '/class/all_classes.php');
 
 
-
-if (!empty($_GET["val"])) {
-    $value = $_GET["val"];
-
-        sscanf($value, "%d", $dg_id);
-
-        $polyprinters="2";
-
-        if ($dg_id == ""){
-            echo "<option selected disabled hidden>Invalid Device Group</option>";
-            
-        } elseif ($dg_id != $polyprinters && DeviceGroup::regexDgID($dg_id)) {
-            // Select all of the MAV IDs that are waiting for this device group
-            $result = $mysqli->query ( "
-                SELECT DISTINCT D.`device_desc`, D.`d_id`
-                FROM `devices` D, `device_group` DG, `transactions` T , `wait_queue` WQ
-                WHERE (D.`dg_id`=$dg_id AND D.`dg_id`=DG.`dg_id` AND T.`d_id`=D.`d_id` AND T.`status_id` <= 11 AND DG.`granular_wait`='Y' AND D.`d_id` NOT IN (
+if ( !empty($_GET["val"]) && DeviceGroup::regexDgID( filter_input(INPUT_GET, "val")) ){
+    $dg = new DeviceGroup( filter_input(INPUT_GET, "val") );
+    if ($dg->getGranular_wait() == "Y"){
+        if ($result = $mysqli->query("
+            SELECT DISTINCT`devices`.`d_id`, `devices`.`device_desc`
+            FROM `devices`
+            LEFT JOIN `service_call`
+            ON `service_call`.`d_id` = `devices`.`d_id`
+            WHERE public_view = 'Y' AND `devices`.`dg_id` = ".$dg->getDg_id()."
+                    AND `devices`.`d_id` NOT IN (
                     SELECT `d_id`
                     FROM `service_call`
                     WHERE `solved` = 'N' AND `sl_id` >= 7
-                )) OR D.`d_id` IN (
-                    SELECT WQ.`Dev_id`
-                    FROM `wait_queue` WQ, `devices` D, `device_group` DG
-                    WHERE D.`dg_id`=$dg_id AND WQ.`Dev_id`=D.`d_id` AND DG.`granular_wait`='Y' AND WQ.`valid` = 'Y' 
                 )
-                ORDER BY D.`device_desc`
-            " );
-    
-            while($row = mysqli_fetch_array($result)) {
-                echo '<option value="'.$row["d_id"].'">'; echo $row["device_desc"]; echo "</option>";
+            ORDER BY `device_desc` ASC
+        ")){
+            while($row = $result->fetch_assoc()){
+                echo "<option value=$row[d_id]>$row[device_desc]</option>";
             }
-            if (mysqli_num_rows($result)==0) {
-                echo ("<script>document.getElementById(\"deviceList\").disabled = true;</script>");
-                echo "<option disabled selected>"; echo "A Device Is Available To Use"; echo "</option>";
-            }
-            
-        
-        } elseif ($dg_id == $polyprinters && DeviceGroup::regexDgID($dg_id)) {
-            $result = $mysqli->query ( "
-                SELECT DISTINCT D.`device_desc`, D.`d_id`
-                FROM `devices` D, `device_group` DG, `transactions` T , `wait_queue` WQ
-                WHERE (D.`dg_id`=$dg_id AND D.`dg_id`=DG.`dg_id` AND T.`d_id`=D.`d_id` AND T.`status_id` <= 11 AND DG.`granular_wait`='N' AND D.`d_id` NOT IN (
-                    SELECT `d_id`
-                    FROM `service_call`
-                    WHERE `solved` = 'N' AND `sl_id` >= 7
-                )) OR D.`d_id` IN (
-                    SELECT WQ.`Dev_id`
-                    FROM `wait_queue` WQ, `devices` D, `device_group` DG
-                    WHERE D.`dg_id`=$dg_id AND WQ.`Dev_id`=D.`d_id` AND DG.`granular_wait`='N' AND WQ.`valid` = 'Y' 
-                )
-                ORDER BY D.`device_desc`
-            " );
-            
-            if (mysqli_num_rows($result)==0) {
-                echo "<option disabled selected>"; echo "A Printer Is Available To Use"; echo "</option>";  
-            } else {
-                echo "<option value=''>"; echo "No Selection Needed"; echo "</option>";
-            }
-        } else {
-            echo "<option selected disabled hidden>Query Error</option>";
         }
-    
-} ?>
+    } else {
+        echo "<option value=''>"; echo "No Selection Needed"; echo "</option>";
+    }
+} else {
+    echo "<option selected disabled hidden>Invalid Device Group</option>";
+}
+
+?>
