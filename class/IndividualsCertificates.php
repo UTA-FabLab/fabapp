6c72011c30a0e3b2eec54553e7a8885055444f3d
mpzinke <mathew.zinke@hotmail.com>
2018-12-03 12:58:22 -0600

    Revert "Updated db and dependencies to use `revoked` instead of `current`"
    This reverts commit 48fc4bf1f1f3d96508b149055ec8b6898ac2c356.

--- class/IndividualsCertificates.php
+++ class/IndividualsCertificates.php
@@ -50,27 +50,29 @@ class IndividualsCertificates {
         if(!preg_match( '/^\d+$/', $tme_key)) return false;
         $reason = htmlspecialchars($reason);
 
-        // check if primary key exists
+        // update reason if reason; check if primary key exists
         $prior_revoke_reason = $mysqli->query("
             SELECT `altered_notes`
             FROM `tm_enroll`
             WHERE `tme_key` = $tme_key;");
         if(!$prior_revoke_reason) return false;
-        $reason = $reason."\n".($prior_revoke_reason->fetch_object()->altered_notes);  // combine reasons; delimiter of '\n'
+        $prior_revoke_reason = $prior_revoke_reason->fetch_object()->altered_notes;
+        if($prior_revoke_reason !== NULL || $prior_revoke_reason != "") $reason = $prior_revoke_reason."\n".$reason;
+
 
         if ($stmt = $mysqli->prepare(" 
             UPDATE `tm_enroll`
-            SET `altered_by` = ?, `altered_date` = now(), `altered_notes`= ?, `revoked` = 'Y', `expiration_date`= ?
+            SET `altered_by` = ?, `altered_date` = now(), `altered_notes`= ?, `current` = 'N', `expiration_date`= ?
             WHERE `tme_key` = ?;
         ")) {
         $stmt->bind_param("sssi", $staff->getOperator(), $reason, $expiration, $tme_key);
         if ($stmt->execute() === true ){
             $row = $stmt->affected_rows;
-            // Success, only one row was updated
+            //Success, only one row was updated
             if ($row == 1){
                 $mysqli->commit();
                 return true;
-            // Error More then one row was affected
+            //Error More then one row was affected
             } elseif ($row > 1) {
                 $mysqli->rollback();
             }
@@ -85,15 +87,9 @@ class IndividualsCertificates {
 
         if(!preg_match( '/^\d+$/', $tme_key)) return false;
         
-        // prevent self reinstallment by admin OPTION FOR THE FUTURE
-        // if($staff_id == $mysqli->query("SELECT `operator`
-        //                                 FROM `tm_enroll`
-        //                                 WHERE `tme_key` = $tme_key;
-        // ")) return false;
-
         if($mysqli->query("
             UPDATE `tm_enroll`
-            SET `revoked` = 'N', `altered_by` = '$staff_id'
+            SET `current` = 'Y', `altered_by` = '$staff_id'
             WHERE `tme_key` = $tme_key;
         ")){
             if($mysqli->affected_rows == 1) {
