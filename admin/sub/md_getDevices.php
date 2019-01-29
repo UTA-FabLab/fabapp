cbc05de9c4334c08a78bceeb9fde8779d83d1cc1
sammyhamwi <sammyhamwi@gmail.com>
2019-01-28 18:00:09 -0600

    Manage Device/Device Group page added with all functionality

---
+++ admin/sub/md_getDevices.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ *   CC BY-NC-AS UTA FabLab 2016-2018
+ *   FabApp V 0.91
+ * 
+ */
+ 
+include_once ($_SERVER ['DOCUMENT_ROOT'] . '/connections/db_connect8.php');
+include_once ($_SERVER ['DOCUMENT_ROOT'] . '/class/all_classes.php');
+
+
+if ( !empty($_GET["val"]) && DeviceGroup::regexDgID( filter_input(INPUT_GET, "val")) ){
+    $dg = new DeviceGroup( filter_input(INPUT_GET, "val") );
+    if ($result = $mysqli->query("
+        SELECT DISTINCT`devices`.`d_id`, `devices`.`device_desc`
+        FROM `devices`
+        WHERE public_view = 'Y' AND `devices`.`dg_id` = ".$dg->getDg_id()."
+        ORDER BY `device_desc` ASC
+    ")){
+        while($row = $result->fetch_assoc()){
+            
+            echo "<option value=$row[d_id]>$row[device_desc]</option>";
+        }
+    }
+} else {
+    echo "<option selected disabled hidden>Invalid Device Group</option>";
+}
+
+?>
