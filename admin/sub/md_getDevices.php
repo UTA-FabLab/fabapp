ec5c8a5520b32970cc112826afd7e834268b3168
sammyhamwi <sammyhamwi@gmail.com>
2019-02-08 02:38:48 -0600

    Edit device page can now edit a device's public view attribute.

--- admin/sub/md_getDevices.php
+++ admin/sub/md_getDevices.php
@@ -11,15 +11,30 @@ include_once ($_SERVER ['DOCUMENT_ROOT'] . '/class/all_classes.php');
 
 if ( !empty($_GET["val"]) && DeviceGroup::regexDgID( filter_input(INPUT_GET, "val")) ){
     $dg = new DeviceGroup( filter_input(INPUT_GET, "val") );
-    if ($result = $mysqli->query("
-        SELECT DISTINCT`devices`.`d_id`, `devices`.`device_desc`
-        FROM `devices`
-        WHERE public_view = 'Y' AND `devices`.`dg_id` = ".$dg->getDg_id()."
-        ORDER BY `device_desc` ASC
-    ")){
-        while($row = $result->fetch_assoc()){
-            
-            echo "<option value=$row[d_id]>$row[device_desc]</option>";
+    if (filter_input(INPUT_GET, "loc") == 1){
+        if ($result = $mysqli->query("
+            SELECT DISTINCT`devices`.`d_id`, `devices`.`device_desc`
+            FROM `devices`
+            WHERE public_view = 'Y' AND `devices`.`dg_id` = ".$dg->getDg_id()."
+            ORDER BY `device_desc` ASC
+        ")){
+            while($row = $result->fetch_assoc()){
+
+                echo "<option value=$row[d_id]>$row[device_desc]</option>";
+            }
+        }
+    }
+    if (filter_input(INPUT_GET, "loc") == 0){
+        if ($result = $mysqli->query("
+            SELECT DISTINCT`devices`.`d_id`, `devices`.`device_desc`
+            FROM `devices`
+            WHERE `devices`.`dg_id` = ".$dg->getDg_id()."
+            ORDER BY `device_desc` ASC
+        ")){
+            while($row = $result->fetch_assoc()){
+
+                echo "<option value=$row[d_id]>$row[device_desc]</option>";
+            }
         }
     }
 } else {
