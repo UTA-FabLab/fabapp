efe4513706920931d79b7c0a653d0a815c2901ff
mpzinke <mathew.zinke@hotmail.com>
2019-03-01 10:11:14 -0600

    Update on Data Reports

--- admin/stats.php
+++ admin/stats.php
@@ -26,92 +26,36 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['prebuilt_button'])) {
 	$function = htmlspecialchars(filter_input(INPUT_POST, "prebuilt_query"));
 	$start = htmlspecialchars(filter_input(INPUT_POST, "start_time"));
 	$end = htmlspecialchars(filter_input(INPUT_POST, "end_time"));
-	echo "<script> console.log('SUCCESS'); </script>";  //TESTING
 
-	if($function === "byHour") {
-		$statement = "SELECT HOUR(`t_start`), COUNT(*)
-	  					FROM `transactions`
-	  					WHERE '$start' <= DATE(`t_start`) 
-	  					AND DATE(`t_start`) <= '$end'
-	  					GROUP BY HOUR(`t_start`);";
-		$file_name = "TicketsByHour";
-	}
-	elseif($function === "byDay") {
-		$statement = "SELECT DAYNAME(`t_start`), COUNT(*)
-						FROM `transactions`
-						WHERE '$start' <= `t_start` 
-	  					AND `t_start` <= '$end'
-						GROUP BY WEEKDAY(`t_start`);";				
-		$file_name = "TicketByDay";
-	}
-	elseif($function === "byHourDay") {
-		$statement = "SELECT HOUR(`t_start`), DAYNAME(`t_start`), COUNT(*)
-	  					FROM `transactions`
-	  					WHERE '$start' <= `t_start` 
-	  					AND `t_start` <= '$end'
-	  					GROUP BY HOUR(`t_start`), WEEKDAY(`t_start`)
-	  					ORDER BY WEEKDAY(`t_start`), HOUR(`t_start`);";
-		$file_name = "TicketsByHourForEachDay";
-	}
-	elseif($function === "byStation") {
-		$statement = "SELECT `device_group`.`dg_desc`, COUNT(*)
-						FROM `transactions`
-						JOIN `devices` ON `transactions`.`d_id` = `devices`.`d_id`
-						JOIN `device_group` ON `devices`.`dg_id` = `device_group`.`dg_id`
-						WHERE '$start' <= `transactions`.`t_start`
-						AND `transactions`.`t_start` <= '$end'
-						GROUP BY `device_group`.`dg_desc`;";
-		$file_name = "TicketsByStation";
-	}
-	elseif($function === "byAccount") {
-		$statement = "SELECT `accounts`.`name`, COUNT(*)
-						FROM `transactions`
-						JOIN `acct_charge` ON `transactions`.`trans_id` = `acct_charge`.`trans_id`
-						JOIN `accounts` ON `acct_charge`.`a_id` = `accounts`.`a_id`
-						WHERE '$start' <= `transactions`.`t_start`
-						AND `transactions`.`t_start` <= '$end'
-						GROUP BY `accounts`.`name`;";
-		$file_name = "TicketsByAccount";
-	}
-	elseif($function === "failedTickets") {
-		$statement = "SELECT COUNT(*)
-						FROM `transactions`
-						WHERE `status_id` = 12
-						AND '$start' <= `t_start` 
-	  					AND `t_start` <= '$end';";
-
-		$file_name = "FailedTickets";
-	}
-	create_pie_chart($file_name, $statement);
-	export_csv($file_name, $statement);
-	echo "<script> console.log('Here1'); </script>";  //TESTING
+	$params = Table::get_prebuild_data($end, $function, $start);
+	
+	$csv = export_csv($params["file_name"], $params["head"], $params["statement"]);
+	if($params["file_name"] != "TicketsByHourForEachDay" && $params["file_name"] != "FailedTickets") 
+		$pie = create_pie_chart($params["file_name"], $params["head"], $params["statement"]);
 }
 
 
-function export_csv($file_name, $statement) {
+
+// -————————————  PREBUILD QUERY ————————————— 
+// 	-Get fields, sort values, inject JS function call at bottom of page
+
+function export_csv($file_name, $head, $statement) {
 	global $mysqli;
 
+	$values = "";
 	if($results = $mysqli->query($statement)) {
-  		ob_end_clean();
-  		ob_clean();
-
-		$file_name .= ".csv";
-		$query_file = fopen("php://output", "w");
-		
-		// write file
-		$row = $results->fetch_assoc();
-		fputcsv($query_file, array_keys($row));
-		fputcsv($query_file, $row);
-		while($row = $results->fetch_assoc()) {
-			fputcsv($query_file, $row, ",");
-		}
-		// export csv
-		header('Content-Type: text/csv; charset=utf-8');
-		header("Content-Disposition: attachment; filename=$file_name");
-
-  		ob_start();
-  		ob_flush();
-		exit();
+  		// failed tickets
+  		if($file_name == "FailedTickets") {
+  			$row = $results->fetch_assoc();
+  			$values = "$head[0]\n".$row['COUNT(*)'];
+  		}
+  		else {
+  			$values = implode(",", $head)."\\n";
+	  		while($row = $results->fetch_assoc()) {
+	  			$values .= implode(",", $row)."\\n";
+	  		}
+	  	}
+	  	return array_combine(array('file_name', 'data'), array($file_name, $values));
 	}
 	else {
 		echo "<script>window.onload = function() {".
@@ -121,7 +65,7 @@ function export_csv($file_name, $statement) {
 }
 
 
-function create_pie_chart($file_name, $statement) {
+function create_pie_chart($file_name, $head, $statement) {
 	global $mysqli;
 
 	if($results = $mysqli->query($statement)) {
@@ -130,15 +74,15 @@ function create_pie_chart($file_name, $statement) {
 				"}; </script>";  // clear possible error message
 
 		$values = array();
-		$row = $results->fetch_assoc();
-		$head = array_keys($row);
-		$values[$head[0]] = $row[$head[1]];
+		$slices = array();
 		while($row = $results->fetch_assoc()) {
-			$values[$head[0]] = $row[$head[1]];
+			$values[$row[$head[0]]] = $row[$head[1]];
 		}
 		$sum = array_sum($values);
-
-		echo "<script> console.log('$sum'); </script>";  //TESTING
+		foreach($values as $key => $value) {
+			$slices[] = "$key,".strval($value / $sum);
+		}
+		return array_combine(array('data', 'filename'), array(implode(";", $slices), $file_name));
 	}
 	else {
 		echo 	"<script>window.onload = function() {".
@@ -147,16 +91,7 @@ function create_pie_chart($file_name, $statement) {
 	}	
 }
 
-
-
-
-// function pie chart
-// download image
-
-// function for each query
-
-//general queries
-
+// -————————————— QUERY BUILDER ————————————— 
 
 $tables = get_tables();
 
@@ -239,7 +174,7 @@ function get_tables() {
 			<div class='col-md-6'>
 				<select id='table_select' class='form-control status_select' onchange='getTableCols(this, "table1")'>
 				<?php
-				if(count($tables) > 0) {
+				if($tables && count($tables) > 0) {
 					echo "<option selected value=''>—Select Table—</option>";
 					foreach($tables as $tab) {
 						echo "<option value='$tab[table_name]'>".$tab['label']."</option>";
@@ -257,7 +192,7 @@ function get_tables() {
 				<select id='cross_reference' class='form-control status_select' onchange='getTableCols(this, "table2")' disabled>
 					<option selected style="color:#888888;">—Optional Cross Reference—</option>
 				<?php
-				if(count($tables) > 0) {
+				if($tables && count($tables) > 0) {
 					foreach($tables as $tab) {
 						echo "<option value='$tab[table_name]'>".$tab['label']."</option>";
 					}
@@ -301,8 +236,15 @@ function get_tables() {
 	}
 
 
-//————————————— Pie Chart —————————————
 
+	function create_csv(filename, array) {
+		var csvContent = 'data:text/csv;charset=utf-8,' + array;
+		var encodedUri = encodeURI(csvContent);
+		window.open(encodedUri, 'test.csv');
+	}
+
+
+// ————————————— Pie Chart —————————————
 
 	function create_wedge(canvas, color, start, stop, text, pos_x, pos_y) {
 		if (canvas.getContext) {
@@ -315,7 +257,7 @@ function get_tables() {
 			ctx.fill();
 			// text
 			ctx.fillStyle = '#000000';
-			ctx.font = 'normal 40px Helvetica';
+			ctx.font = 'normal 30px Helvetica';
 			ctx.fillText(text, pos_x+50, pos_y);
 		}
 	}
@@ -326,7 +268,7 @@ function get_tables() {
 		element.href = image;
 	}
 
-	function create_pie_chart(data, data_labels) {
+	function create_pie_chart(data, filename) {
 		var canvas = document.getElementById('piechart');
 		canvas.getContext('2d').clearRect(0, 0, 1000, 1500);
 
@@ -334,21 +276,24 @@ function get_tables() {
 					    '#CB331F', '#800000', '#652121', '#5C4033', '#333333', '#999999', '#FF69B4', '#CB3464', '#CB3496', '#CA3AE7', '#8129F5',
 					    '#551A8B', '#000080'];
 		var start = 0;
+		data = data.split(';');
 		var row_per_col = Math.ceil(data.length/3);
 		for(var x = 0; x < data.length; x++) {
 			// chart
-			var stop = start + 2 * Math.PI * data[x];
+			var row = data[x].split(',');
+			var stop = start + 2 * Math.PI * row[1];
 			var color = colors[parseInt(x*colors.length/data.length)];
 			var text_pos = [parseInt(x/row_per_col)*300+50, (x%row_per_col)*50+1100];
-			create_wedge(canvas, color, start, stop, data_labels[x], text_pos[0], text_pos[1]);
+			create_wedge(canvas, color, start, stop, row[0], text_pos[0], text_pos[1]);
 			start = stop;
 		}
+		document.getElementById('download').download = filename;
 		document.getElementById('download').click();
 	}
 
 
 
-//———————————— Query Builder ————————————
+// ———————————— Query Builder ————————————
 
 
 	// AJAX for getting the names of columns from selected table
@@ -373,7 +318,7 @@ function get_tables() {
 
 
 	function isChecked(x) {
-		console.log("HERE");
+		console.log("HERE");  //TESTING
 		if( $("#"+x).is(":visible") ) 
 			$("#"+x).hide();
 		else {
@@ -382,3 +327,13 @@ function get_tables() {
 	}
 
 </script>
+
+<?php 
+// ugly call to JS to print/download everything
+if(isset($csv)) {
+	echo "<script> create_csv('$csv[file_name]', '$csv[data]'); </script>";
+	if(isset($pie)) {
+		echo "<script> create_pie_chart('$pie[data]', '$pie[filename]'); </script>";
+	}
+}
+?>
