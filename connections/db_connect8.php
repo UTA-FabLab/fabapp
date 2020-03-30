edbe739ffa47eb1782a069942c5bf6cea4d968a6
FabosaurusRex <olson@uta.edu>
2020-03-30 14:02:35 -0500

    Updating db8_connect for current format
    Scrubbed out user/host/other sensitive data, will be filling in manually from now on when uploading

--- connections/db_connect8.php
+++ connections/db_connect8.php
@@ -1,5 +1,11 @@
 
-<?php
-    // Connecting to mysql database with Role 8
-    $mysqli = new mysqli('dbhost', 'dbuser', 'dbpass', 'fabapp') or die(mysql_error());
+$dbhost = "";
+$dbuser = "";
+$dbpass = "";
+$dbdatabase = "";
+// Connecting to mysql database
+$mysqli = new mysqli($dbhost, $dbuser, $dbpass) or die(mysql_error());
+
+// Selecting database 
+$mysqli->select_db($dbdatabase) or die(mysql_error());
 ?>
