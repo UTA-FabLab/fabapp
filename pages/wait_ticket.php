55d3a7a27efe2f74ef258219c29f484487808f51
MickPoletti <mmcdhunter1@yahoo.com>
2020-07-11 13:25:20 -0500

    Move alerts into alert variables
    Shift the wait ticket code to pull the notifications' verbage from site variables.

--- pages/wait_ticket.php
+++ pages/wait_ticket.php
@@ -8,6 +8,29 @@ $d_id1 = $dg_id1 = $ph1 = $carrier_name1 = $em1 = $operator1 = $wt_msg = "";
 $number_of_queue_tables = 0;
 $device_array = array();
 
+// Notifications
+
+$operatorInQueueAlert = ("<div style='text-align: center'>
+                    <div class='alert alert-danger'>
+                        Operator is already in this Wait Queue.
+                    </div> </div>");
+$successAlert = ("<div style='text-align: center'>
+            <div class='alert alert-success'>
+                Successfully added to wait queue and updated contact info!
+            </div> </div>");
+$phoneNumberAlert = ("<div style='text-align: center'>
+                <div class='alert alert-danger'>
+                    You must enter a phone Number if selecting a carrier!
+                </div> </div>");
+$carrierAlert = ("<div style='text-align: center'>
+                <div class='alert alert-danger'>
+                    You must select a carrier if entering a phone number!
+                </div> </div>");
+$selectDeviceAlert = ("<div style='text-align: center'>
+                <div class='alert alert-danger'>
+                    You must select a device!
+                </div> </div>");
+
 if (!$staff || $staff->getRoleID() < $sv['LvlOfStaff']){
     //Not Authorized to see this Page
     $_SESSION['error_msg'] = "You are unable to view this page.";
@@ -15,10 +38,7 @@ if (!$staff || $staff->getRoleID() < $sv['LvlOfStaff']){
     exit();
 }
 if (isset($_SESSION['wt_msg']) && $_SESSION['wt_msg'] == 'success'){
-    $wt_msg = ("<div style='text-align: center'>
-            <div class='alert alert-success'>
-                Successfully added to wait queue and updated contact info!
-            </div> </div>");
+    $wt_msg = $successAlert;
     unset($_SESSION['wt_msg']);
 }
 if (!array_key_exists("clear_queue",$sv)){
@@ -40,21 +60,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['removeBtn']) && $staff
         
         if ($wait_status)
         {
-            $wt_msg = ("<div style='text-align: center'>
-                    <div class='alert alert-danger'>
-                        Operator is already in this Wait Queue.
-                    </div> </div>");
+            $wt_msg = $operatorInQueueAlert;
         } else {
             if($ph1 && !isset($_POST['carrier_name'])){
-                $wt_msg = ("<div style='text-align: center'>
-                <div class='alert alert-danger'>
-                    You Must Select A Carrier If Entering a Phone Number
-                </div> </div>");
+                $wt_msg = $carrierAlert;
             } elseif (!$ph1 && isset($_POST['carrier_name'])){
-                $wt_msg = ("<div style='text-align: center'>
-                <div class='alert alert-danger'>
-                    You Must Enter a Phone Number If Selecting a Carrier
-                </div> </div>");
+                $wt_msg = $phoneNumberAlert;
                 
             } else {
                 $carrier_name1 = filter_input(INPUT_POST,'carrier_name');
@@ -70,10 +81,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['removeBtn']) && $staff
             }
         }
     } else {
-        $wt_msg = ("<div style='text-align: center'>
-                <div class='alert alert-danger'>
-                    You Must Select A Device
-                </div> </div>");
+        // devices and device group set this line is unreachable.
+        $wt_msg = $selectDeviceAlert;
     }
 }
 ?>
