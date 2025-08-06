50a9029d3d931c51396b4527be465b508f4da066
FabosaurusRex <olson@uta.edu>
2025-08-06 15:17:41 -0500

    Add IDT handling option
    Adding in logic to handle IDT requests and redirect to temporary e-Form site.

--- pages/pay.php
+++ pages/pay.php
@@ -476,12 +476,43 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/pages/footer.php');
 	}
 
 
-	function adjust_submit_button_for_payment_type(payment_select) {
+/*	function adjust_submit_button_for_payment_type(payment_select) {
 		var button = document.getElementById("pay_button")
 		// pay site
 		if(payment_select.value == 2 && <?php echo $ticket->remaining_balance() ?>) {
 			button.innerHTML = "Launch <?php echo $sv['paySite_name'];?>";
 			button.onclick = launch_pay_site;
+			button.type = "button";
+		}
+		else {
+	            button.classList.toggle("btn-danger");
+			button.type = "submit";
+			button.innerHTML = "Submit";
+			button.onclick = "";
+		}
+	}
+	//old function preserved for now, just in case
+*/
+
+function adjust_submit_button_for_payment_type(payment_select) {
+		var button = document.getElementById("pay_button")
+		// pay site
+		console.log("Value of title property: ");
+		
+//		let textExtract = payment_select.options[payment_select.selectedIndex];		//testing
+		
+//		console.log(textExtract.text);
+		console.log(payment_select.options[payment_select.selectedIndex].text);		//testing
+		
+		if(payment_select.value == 2 && <?php echo $ticket->remaining_balance() ?>) {
+			button.innerHTML = "Launch <?php echo $sv['paySite_name'];?>";
+			button.onclick = launch_pay_site;
+			button.type = "button";
+		}
+		else if( payment_select.options[payment_select.selectedIndex].text == "IDT" && <?php echo $ticket->remaining_balance() ?>)		//this pulls the options of payment_select object 
+		{																																//and turns them into an array, then looks at the 
+			button.innerHTML = "Launch <?php echo $sv['IDTSite_name'];?>";																//array with index value corresponding to the drop-down's index value,
+			button.onclick = launch_IDT_site;																							//then extracts the text value of it for evaluation
 			button.type = "button";
 		}
 		else {
@@ -501,6 +532,18 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/pages/footer.php');
             button.type = "submit";
             button.classList.add("btn-danger");
             button.innerHTML = "Confirm Payment";
+            button.onclick = null;
+	}
+	
+
+	// open window for temporary IDT site 
+	function launch_IDT_site() {
+            var button = document.getElementById("pay_button");
+
+            pay_window = window.open("<?php echo $sv['IDTSite'];?>", "pay_window", "top=100,width=750,height=500");
+            button.type = "submit";
+            button.classList.add("btn-danger");
+            button.innerHTML = "Confirm IDT Payment";
             button.onclick = null;
 	}
 
